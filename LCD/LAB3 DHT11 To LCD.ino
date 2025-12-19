c7c48c95f6629352f3848a0fce3db15ba9b50a89
pitikornchaitong2007 <pitikornchaitong2007@gmail.com>
2025-12-19 16:30:55 +0700

    Update LAB3 DHT11 To LCD.ino

--- LCD/LAB3 DHT11 To LCD.ino
+++ LCD/LAB3 DHT11 To LCD.ino
@@ -1,43 +1,67 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
-// LCD I2C
+// ===== LCD =====
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// ขา LM35
-const int tempPin = A0;
+// ===== DHT11 =====
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  lcd.init();
+  Serial.begin(9600);
+
+  lcd.begin(16, 2);
   lcd.backlight();
 
-  lcd.setCursor(0, 0);
-  lcd.print("LCD TEST OK");
-  lcd.setCursor(0, 1);
-  lcd.print("Temp System");
+  dht.begin();
 
+  lcd.setCursor(0, 0);
+  lcd.print("LAB3 DHT11 LCD");
+  lcd.setCursor(0, 1);
+  lcd.print("Starting...");
+  Serial.println("System Starting...");
   delay(2000);
   lcd.clear();
 }
 
 void loop() {
-  // อ่านค่าอุณหภูมิ
-  int sensorValue = analogRead(tempPin);
-  float voltage = sensorValue * (5.0 / 1023.0);
-  float temperature = voltage * 100.0; // LM35 = 10mV/°C
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // Celsius
 
-  static int count = 0;
+  if (isnan(h) || isnan(t)) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DHT Error");
 
+    Serial.println("Failed to read from DHT sensor!");
+    delay(1000);
+    return;
+  }
+
+  // ===== LCD =====
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
-  lcd.print(temperature, 1);
-  lcd.print((char)223); // สัญลักษณ์ °
+  lcd.print(t, 1);
+  lcd.print((char)223);
   lcd.print("C");
 
   lcd.setCursor(0, 1);
-  lcd.print("Count: ");
-  lcd.print(count++);
+  lcd.print("Hum : ");
+  lcd.print(h, 0);
+  lcd.print(" %");
 
-  delay(1000);
+  // ===== Serial =====
+  Serial.print("Temperature: ");
+  Serial.print(t, 1);
+  Serial.print(" C\t");
+
+  Serial.print("Humidity: ");
+  Serial.print(h, 0);
+  Serial.println(" %");
+
+  delay(2000);
 }
