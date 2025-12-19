ed0b4b07d2a2798d08da2d00ea5840f8f50f1371
pitikornchaitong2007 <pitikornchaitong2007@gmail.com>
2025-12-19 16:26:19 +0700

    Update LAB2_Hello World.ino

--- LCD/LAB2_Hello World.ino
+++ LCD/LAB2_Hello World.ino
@@ -1,29 +1,32 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// ประกาศ lcd ไว้ตรงนี้ ต้องอยู่นอก setup/loop
+// ประกาศ lcd
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   lcd.init();        // เริ่มต้นจอ
   lcd.backlight();   // เปิดไฟพื้นหลัง
 
+  // แสดง Hello World
   lcd.setCursor(0, 0);
-  lcd.print("LCD TEST OK");
-
+  lcd.print("Hello World");
   lcd.setCursor(0, 1);
-  lcd.print("I2C 16x2");
+  lcd.print("LCD 16x2");
+
+  delay(2000);       // แสดง 2 วินาที
+  lcd.clear();
 }
 
 void loop() {
-  lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Running...");
+  lcd.print("Running...   ");
 
   static int count = 0;
   lcd.setCursor(0, 1);
   lcd.print("Count: ");
   lcd.print(count++);
+  lcd.print("   ");
 
   delay(1000);
 }
