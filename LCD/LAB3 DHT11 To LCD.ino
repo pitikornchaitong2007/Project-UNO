dfcfb14b53c818ec056900c928112d014ca0a57f
pitikornchaitong2007 <pitikornchaitong2007@gmail.com>
2025-12-19 16:28:54 +0700

    Update LAB3 DHT11 To LCD.ino

--- LCD/LAB3 DHT11 To LCD.ino
+++ LCD/LAB3 DHT11 To LCD.ino
@@ -1,26 +1,40 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// ประกาศ lcd ไว้ตรงนี้ ต้องอยู่นอก setup/loop
+// LCD I2C
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// ขา LM35
+const int tempPin = A0;
+
 void setup() {
-  lcd.init();        // เริ่มต้นจอ
-  lcd.backlight();   // เปิดไฟพื้นหลัง
+  lcd.init();
+  lcd.backlight();
 
   lcd.setCursor(0, 0);
   lcd.print("LCD TEST OK");
-
   lcd.setCursor(0, 1);
-  lcd.print("I2C 16x2");
+  lcd.print("Temp System");
+
+  delay(2000);
+  lcd.clear();
 }
 
 void loop() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Running...");
+  // อ่านค่าอุณหภูมิ
+  int sensorValue = analogRead(tempPin);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  float temperature = voltage * 100.0; // LM35 = 10mV/°C
 
   static int count = 0;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature, 1);
+  lcd.print((char)223); // สัญลักษณ์ °
+  lcd.print("C");
+
   lcd.setCursor(0, 1);
   lcd.print("Count: ");
   lcd.print(count++);
